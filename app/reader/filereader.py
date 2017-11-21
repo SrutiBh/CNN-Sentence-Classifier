fb265ca008eea831f42ee8c55d82b8f648fb17aa
Sruti Bhattacharjee <SrutiBh@users.noreply.github.com>
2017-11-22 03:02:22 +0530

    Update filereader.py

--- app/reader/filereader.py
+++ app/reader/filereader.py
@@ -13,7 +13,7 @@ def read_glove_vectors(glove_vector_path):
         for line in f:
             values = line.strip().split()
             word = values[0]
-            coefs = np.asarray(values[1:], dtype='float32')
+            coefs = np.asarray(values[ 1: ], dtype=' float32 ')
             embeddings_index[word] = coefs[:]
     return embeddings_index
 
@@ -31,7 +31,7 @@ def read_input_data(input_data_path):
             label = str(line.strip())
             if label not in labels_index:
                 labels_index[label] = largest_label_id
-                largest_label_id += 1
+                largest_label_id + = 1
             labels.append(labels_index[label])
 
     return texts, labels_index, labels
